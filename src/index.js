681c7691c3047baca2401c2c400010c412916ce0
DzmitryKas <kasperovich_dima@mail.ru>
2020-03-06 15:38:54 +0300

    add slider

--- src/index.js
+++ src/index.js
@@ -2,9 +2,33 @@ import * as $ from 'jquery'
 import './styles/main.scss'
 import Swiper from 'swiper';
 
+import './js/header.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var mySwiper = new Swiper('.swiper-container', { 
+        spaceBetween: 30,
+        centeredSlides: true,
+        loop: true,
+        slidesPerView: 1,
+        breakpoints: {
+            1600: {
+              slidesPerView: 2,
+            }
+          },
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+    });
+    console.log(mySwiper)
+ }, false);
 
 
-import './js/header.js'
-
-var mySwiper = new Swiper('.swiper-container', { /* ... */ });
-console.log(Swiper)
